a1ecf99414fe0aabc778cfe4f7fc0ee5b2c5d90f
Micah Tam <micahtam70@gmail.com>
2026-03-07 15:54:28 -0500

    fixed login/registration jitteriness

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -3,9 +3,24 @@ import { Stack } from "expo-router";
 
 export default function AuthLayout() {
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="login" />
-      <Stack.Screen name="register" />
+    <Stack
+      screenOptions={{
+        headerShown: false,
+        gestureEnabled: false,
+      }}
+    >
+      <Stack.Screen name="login" options={{ title: "" }} />
+      <Stack.Screen
+        name="register"
+        options={{
+          headerShown: true,
+          headerTitle: "",
+          headerBackTitle: "",
+          headerTransparent: true,
+          headerShadowVisible: false,
+          gestureEnabled: true,
+        }}
+      />
     </Stack>
   );
 }
